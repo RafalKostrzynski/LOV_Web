1ec2dea8d1e7a4590b9a6e851335c6c008d5fdf4
RafalKostrzynski <rafx97@gmail.com>
2020-03-27 10:26:41 +0100

    LW-35 Lista do wyświetlania

--- src/main/java/com/lov/lovwebapp/security/WebSecurityConfig.java
+++ src/main/java/com/lov/lovwebapp/security/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
